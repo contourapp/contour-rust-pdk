463968ea8630b106181e8cd58ca8ee5787458025
Alex Propp <alexpropp@gmail.com>
2024-05-09 00:42:11 +0000

    Merging upsert and insert

--- src/lib.rs
+++ src/lib.rs
@@ -31,14 +31,9 @@ extern "ExtismHost" {
     fn query_tag_host(input: String) -> String;
     fn query_tags_host(input: String) -> String;
     fn insert_agent_host(input: String) -> String;
-    fn insert_entry_host(input: String) -> String;
-    fn insert_lines_host(input: String) -> String;
     fn insert_resource_host(input: String) -> String;
     fn insert_tag_host(input: String) -> String;
-    fn upsert_agent_host(input: String) -> String;
-    fn upsert_entry_and_lines_host(input: String) -> String;
-    fn upsert_resource_host(input: String) -> String;
-    fn upsert_tag_host(input: String) -> String;
+    fn insert_entry_and_lines_host(input: String) -> String;
     fn scrape_sierrapay_host(input: String) -> String;
     fn scrape_cryptopay_host(input: String) -> String;
     fn scrape_everwash_subscribers_host(input: String) -> String;
@@ -62,14 +57,9 @@ pub mod host_fns {
         pub fn query_tag_host(input: String) -> Result<String>;
         pub fn query_tags_host(input: String) -> Result<String>;
         pub fn insert_agent_host(input: String) -> Result<String>;
-        pub fn insert_entry_host(input: String) -> Result<String>;
-        pub fn insert_lines_host(input: String) -> Result<String>;
         pub fn insert_resource_host(input: String) -> Result<String>;
         pub fn insert_tag_host(input: String) -> Result<String>;
-        pub fn upsert_agent_host(input: String) -> Result<String>;
-        pub fn upsert_entry_and_lines_host(input: String) -> Result<String>;
-        pub fn upsert_resource_host(input: String) -> Result<String>;
-        pub fn upsert_tag_host(input: String) -> Result<String>;
+        pub fn insert_entry_and_lines_host(input: String) -> Result<String>;
         pub fn scrape_sierrapay_host(input: String) -> Result<String>;
         pub fn scrape_cryptopay_host(input: String) -> Result<String>;
         pub fn scrape_everwash_subscribers_host(input: String) -> Result<String>;
@@ -159,26 +149,8 @@ pub fn insert_tag<T: Serialize>(input: io::InputTag<T>) -> Result<Uuid> {
     Ok(output)
 }
 
-pub fn upsert_agent<A: Serialize>(input: io::InputAgent<A>) -> Result<Uuid> {
-    let result = unsafe { upsert_agent_host(serde_json::to_string(&input)?)? };
-    let output = Uuid::from_str(&result).map_err(|_| anyhow::anyhow!("Invalid UUID"))?;
-    Ok(output)
-}
-
-pub fn upsert_entry_and_lines<E: Serialize>(input: io::InputEntryAndLines<E>) -> Result<Uuid> {
-    let result = unsafe { upsert_entry_and_lines_host(serde_json::to_string(&input)?)? };
-    let output = Uuid::from_str(&result).map_err(|_| anyhow::anyhow!("Invalid UUID"))?;
-    Ok(output)
-}
-
-pub fn upsert_resource<R: Serialize>(input: io::InputResource<R>) -> Result<Uuid> {
-    let result = unsafe { upsert_resource_host(serde_json::to_string(&input)?)? };
-    let output = Uuid::from_str(&result).map_err(|_| anyhow::anyhow!("Invalid UUID"))?;
-    Ok(output)
-}
-
-pub fn upsert_tag<T: Serialize>(input: io::InputTag<T>) -> Result<Uuid> {
-    let result = unsafe { upsert_tag_host(serde_json::to_string(&input)?)? };
+pub fn insert_entry_and_lines<E: Serialize>(input: io::InputEntryAndLines<E>) -> Result<Uuid> {
+    let result = unsafe { insert_entry_and_lines_host(serde_json::to_string(&input)?)? };
     let output = Uuid::from_str(&result).map_err(|_| anyhow::anyhow!("Invalid UUID"))?;
     Ok(output)
 }
